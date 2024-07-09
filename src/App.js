fd27daa7d77cb6d53cd40fd87a142c41602b615c
elizabethmoon37 <elizabethmoon37@gmail.com>
2024-07-09 18:19:26 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://github.com/elizabethmoon37"
+          target="blank"
+          rel="noreferrer"
+        >
+          Beth Moon
+        </a>
+        and is
+        <a
+          href="https://github.com/elizabethmoon37/weather-react"
+          target="blank"
+          rel="noreferrer"
+        >
+          Open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
