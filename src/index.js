2ba2d215fae0a6528020f1747928399199feaa29
elizabethmoon37 <elizabethmoon37@gmail.com>
2024-07-09 18:09:45 +0100

    Prepared folders for final react project layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import WeatherSearch from "./WeatherSearch";
+import "./index.css";
+import App from "./App";
+///import * as serviceWorker from ".serviceWorker";
 
 import "./App.css";
 
