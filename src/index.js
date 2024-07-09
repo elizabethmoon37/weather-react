8a8cdf08ed64e627f516c7373b077f8a2aae8d7b
elizabethmoon37 <elizabethmoon37@gmail.com>
2024-07-09 18:49:49 +0100

    Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+
 ///import * as serviceWorker from ".serviceWorker";
 
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-      <br />
-      <small>
-        <a
-          href="https://github.com/elizabethmoon37/weather-react"
-          target="blank"
-          rel="noreferrer"
-        >
-          Open-sourced code{" "}
-        </a>{" "}
-        by
-        <a
-          href="https://github.com/elizabethmoon37"
-          target="blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Beth Moon{" "}
-        </a>
-      </small>
-    </div>
-  );
+  return Hello;
 }
 
 const rootElement = document.getElementById("root");
